7c257e986f2259e6158d54cb03089d8765d5a5df
Jack Bennett <jbennett3@laurentian.ca>
2022-11-29 13:35:38 -0500

    Update Card.h

--- Card.h
+++ Card.h
@@ -5,22 +5,61 @@
 #ifndef HEARTS_CARD_H
 #define HEARTS_CARD_H
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 class Card {
 
     public:
-        Card(); // default constructor
-        Card(int, int); // constructor
-        void setSuit(int); // sets the suit of the card
-        void setRank(int); // sets the rank of the card
-        int getSuit(); // returns the suit of the card
-        int getRank(); // returns the rank of the card
+        enum Suit{Spade, Heart, Club, Diamond, None}; //none is used for Getcard method
+
+        static const string SuitName; //variable for Suit name
+        static const int numSuits; //variable for number of suits
+        static const int numCards; //variable for number of cards
+
+        //Ranking of cards
+        enum Rank{Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten,
+                Jack, Queen, King, Ace}; //Declaring the rank of the card
+
+        static const string RankName; //variable for names of rank of cards
+        static const int NumRanks;   //variable for number of ranks
+
+        
 
 
+        //Defualt constructor
+        Card();
+        //constructor for rank and suit
+        Card(Rank r, Suit s);
+        //constructor taking string as input
+        Card(string sCard);
+        //Deconstructor
+        ~Card();
 
+        //getters
+        Rank getRank() const; //gets rank of card
+        Suit getSuit() const; //gets suit of card
+        int getValueHeart() const; //gets value of card for hearts
+
+        //checks if card is valid
+        static bool isValid(string v);
+
+
+    private:
+        //variables for rank and suit
+        Rank rank;
+        Suit suit;
+        int valueHeart;
 
 
 };
+
+
+//overload operator for printing the card
+ostream& operator<<(ostream& os, const Card& c);
+
+
 
 
 #endif //HEARTS_CARD_H
