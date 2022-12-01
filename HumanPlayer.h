9bba6431c6f877df716167de4b9abaa1f02f9aaa
Jack Bennett <jbennett3@laurentian.ca>
2022-11-30 22:28:00 -0500

    Update HumanPlayer.h

--- HumanPlayer.h
+++ HumanPlayer.h
@@ -2,12 +2,21 @@
 // Created by Jack Bennett on 2022-11-29.
 //
 
+
 #ifndef HEARTS_HUMANPLAYER_H
 #define HEARTS_HUMANPLAYER_H
+#include "Player.h"
 
+class HumanPlayer : public Player {
+private:
+    //virtual functions for player / computer picking card
+     Card leadingCrd() = 0; // picking the leading card
+     Card followingSuit(Card) = 0;  // picking the following suit matching card
+     Card  noSuit() = 0;  //picks any card if no matching suit is available
 
-class HumanPlayer {
-
+public:
+    //constructor
+    HumanPlayer( const string Name);
 };
 
 
