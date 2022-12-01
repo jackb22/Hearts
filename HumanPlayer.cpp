f7ce10b994be35e19e565dcf6259759786a70591
Jack Bennett <jbennett3@laurentian.ca>
2022-12-01 16:26:17 -0500

    Update HumanPlayer.cpp

--- HumanPlayer.cpp
+++ HumanPlayer.cpp
@@ -5,6 +5,8 @@
 
 #include "HumanPlayer.h"
 
+extern Card leadingTrmpCard; //card object for leading trump card
+
 //human player constructor
 HumanPlayer::HumanPlayer(string Name) : Player(Name) {}
 
@@ -72,30 +74,78 @@ Card HumanPlayer::noSuit() {
 }
 
 
+
 //lets player pick a card that is on suit
 Card HumanPlayer::followingSuit(Card card) {
     string tmpcard; //temp string to hold card
+    bool foundSuit = false; //bool to check if player has card  on leadcrd.
+    int index =-1; // used for finding player card
+    //printing current hand of human player
+    cout << "Your hand: " << endl;
+    this->print();
 
-    //taking in card from user / validating input
-    while(true){
-        cout << Name << " Pick a card you want to play: " << endl;
-        cin >> tmpcard;
-        if (!Card::isValid(tmpcard)) {
-            cout << "Invalid card, try again" << endl;
-            cin >> tmpcard;
-        } else{
-            Card result(tmpcard);
-            for (int i = 0; i < size(); i++) {
-                if (result == *at(i)) {
-                    result = *at(i);
-                    cout << Name << " played: " << result << endl;
-                    erase(begin() + i);
-                    return result;
-                }
-
-            }
-            cout<<result<<" is no longer in your hand! "<<endl;
+    //check if the player hand has same suit as lead card
+    for (int i = 0; i < size(); i++) {
+        if (at(i)->getSuit() == leadingTrmpCard.getSuit()) {
+            foundSuit = true;
+            break;
         }
     }
+
+    //if the player does not have the same suit as lead card
+    if(!foundSuit){
+        cout<<"You don't have any cards of the same suit as the lead card"<<endl;
+        return HumanPlayer::noSuit();
+    }
+    else{
+        //taking the card input
+        while(true){
+              cout<< Name << " Pick a card you want to play: " << endl;
+              cin >> tmpcard;
+
+              //if the card is not valid
+            if(!Card::isValid(tmpcard)){
+                cout<<"Invalid card, try again"<<endl;
+                cin>>tmpcard;
+            }
+            else{
+                //if the card is valid
+                Card result(tmpcard);
+                // finding the card in the hand
+                for (int i = 0; i < size(); i++){
+                    if(result == *at(i)){
+                        index = i;
+                        break;
+
+                    }
+
+                }
+                //if the card is not in the hand
+                if(index == -1){
+                    cout<<result<<" is no longer in your hand! "<<endl;
+                }
+
+                //human player has the card as the input
+                else{
+                    //if the player has the same suit as the lead card
+                    if(result.getSuit() != leadingTrmpCard.getSuit()){
+                        cout<<" You have one card of the same suit as the lead card, try again"<<Card::SuitName[leadingTrmpCard.getSuit()]<<endl;
+                        index=-1;
+                    }
+                    else{
+                        //removing the card from the hand
+                            result = *at(index);
+                            cout << Name << " played: " << result << endl;
+                            erase(begin() + index);
+                            return result;
+                    }
+                   }
+            }
+        }
+    }
+
+
+
 }
+
 
