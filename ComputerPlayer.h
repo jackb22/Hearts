b36246f496dda5dae4ee1ea08737debe6f6bb267
Jack Bennett <jbennett3@laurentian.ca>
2022-12-01 20:37:42 -0500

    Update ComputerPlayer.h

--- ComputerPlayer.h
+++ ComputerPlayer.h
@@ -2,13 +2,22 @@
 // Created by Jack Bennett on 2022-11-29.
 //
 
-#ifndef HEARTS_COMPUTERPLAYER_H
-#define HEARTS_COMPUTERPLAYER_H
-
-
-class ComputerPlayer {
-
-};
-
-
-#endif //HEARTS_COMPUTERPLAYER_H
+//#ifndef HEARTS_COMPUTERPLAYER_H
+//#define HEARTS_COMPUTERPLAYER_H
+//#include "Player.h"
+//
+//class ComputerPlayer : public Player {
+//private:
+//    Card leadingCrd() = 0; // picking the leading card
+//    Card followingSuit(Card) = 0;  // picking the following suit matching card
+//    Card  noSuit() = 0;  //picks any card if no matching suit is available
+//
+//public:
+//
+//    //constructor
+//    ComputerPlayer( );
+//
+//};
+//
+//
+//#endif //HEARTS_COMPUTERPLAYER_H
