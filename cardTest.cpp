2cb4e56b0a4ad13c526623feda1724270528a808
Nikholas Pcenicni <82239765+nikpcenicni@users.noreply.github.com>
2022-12-01 21:14:26 -0500

    Created unit Tests for card and deck

---
+++ cardTest.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nikholas Pcenicni on 2022-12-01.
+//
+#include "Card.h"
+
--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 #include "Card.h"
+#include "Deck.h"
+
+
+void cardTest() {
+    Card c1 = Card();
+    Card c2 = Card("QH");
+//    Card c3 = Card(Card::Rank[5], Card::Suit[2]);
+
+    std::cout << c1 << "\n";
+    std::cout << c2 << "\n";
+//    std::cout << c3 << "\n";
+}
+
+void deckTest() {
+    Deck d1 = Deck();
+
+    d1.print();
+}
 
 int main() {
     std::cout << Card();
     std::cout << "Hello, World!" << std::endl;
+    cardTest();
+    deckTest();
     return 0;
 }
