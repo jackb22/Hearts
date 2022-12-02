0c98bfbc2c38465406a30f74cbeb0b61fc8905e8
Jack Bennett <jbennett3@laurentian.ca>
2022-12-01 20:16:21 -0500

    Update Deck.cpp

--- Deck.cpp
+++ Deck.cpp
@@ -2,19 +2,21 @@
 // Created by Jack Bennett on 2022-11-29.
 //
 
+
 #include "Deck.h"
 #include "Card.h"
+#include "Card.cpp"
 #include<vector>
 
-const string RankName = "23456789TJQKA";
-const string SuitName = "SDCH";
+const std::string RankName = "23456789TJQKA";
+const std::string SuitName = "SDCH";
 
-using namespace std;
+
 //constructor and deconstruct method calls
 Deck::Deck(){
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 13; j++) {
-            string sCard = to_string(RankName[j]) + SuitName[i];
+            string sCard = std::to_string(RankName[j]) + SuitName[i];
             this->push_back(new Card(static_cast<Card::Rank>(j), static_cast<Card::Suit>(i)));
         }
     }
@@ -37,12 +39,12 @@ void Deck::reset(bool fullDeck) {
     //create new deck
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 13; j++) {
-            string sCard = to_string(RankName[j]) + SuitName[i];
+            std::string sCard = std::to_string(RankName[j]) + SuitName[i];
             this->push_back(new Card(static_cast<Card::Rank>(j), static_cast<Card::Suit>(i)));
         }
     }
 
-    Deck::shuffleDeck(); // shuiffle deck function
+
 
 }
 
@@ -53,16 +55,13 @@ Deck::Deck(bool fullDeck){
 }
 
 
-
-
-
 //shuffle deck
 void Deck::shuffleDeck(){
     //using time to seed the random number generator
-    srand((unsigned) time(nullptr));
+    std::srand((unsigned) time(nullptr));
 
     // Get a random number
-    int random = rand();
+    int random = std::rand();
 
     //for loop for shuffling the deck based on size of deck and random number
     for (int i = size() - 1; i > 0; i--) {
@@ -74,14 +73,18 @@ void Deck::shuffleDeck(){
 
  }
 
-// print deck method.
-// prints card rank and card suit from Card Class 
+
+
 void Deck::print() const {
     for(unsigned int i = 0; i < size(); i++){
-       cout << Card::RankName[(*this)[i]->getRank()] << Card::SuitName[(*this)[i]->getSuit()] << " ";
+       std::cout << Card::RankName[(*this)[i]->getRank()] << Card::SuitName[(*this)[i]->getSuit()] << " ";
     }
-    cout<<endl;
+    std::cout<<endl;
 }
+
+
+
+
 
 
 
