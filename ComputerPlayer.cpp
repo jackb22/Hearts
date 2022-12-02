7a68768d4aaaa59b41faaa711b3b90fcf75ae183
Jack Bennett <jbennett3@laurentian.ca>
2022-12-01 20:37:00 -0500

    Update ComputerPlayer.cpp

--- ComputerPlayer.cpp
+++ ComputerPlayer.cpp
@@ -2,4 +2,10 @@
 // Created by Jack Bennett on 2022-11-29.
 //
 
-#include "ComputerPlayer.h"
+//#include "ComputerPlayer.h"
+//extern Card leadingTrmpCard; //card object for leading trump card
+//
+////constructor
+//ComputerPlayer::ComputerPlayer() : Player(Name) {
+//
+//}
